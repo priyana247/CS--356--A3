179959f8ff61b2b8a3288c5bf6461261d9841d27
Priyanka Patel <priyankap@cpp.edu>
2017-07-18 22:56:17 -0700

    Add files via upload

---
+++ Group.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+/*
+ *  Group class
+ */
+public class Group {
+  
+  private TwitterDB db;
+  
+  private String id;
+  private HashMap<String,User> users;
+  private long creationTime;
+
+  // Constructor
+  public Group(String id) {
+    this.db = TwitterDB.getInstance();
+    this.id = id;
+    this.users = new HashMap<String,User>();
+    this.creationTime = System.currentTimeMillis();
+  }
+  
+  // Add User to Group
+  public boolean addUser(String user) {
+    if (db.containsUser(user)) {
+      users.put(user,db.getUser(user));
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  // Get Users in Group
+  public List<User> getUsers() {
+    List<User> result = new ArrayList<User>();
+    for (String user: users.keySet()) {
+      result.add(users.get(user));
+    }
+    return result;
+  }
+  
+  // Get Group creation time
+  public long getCreationTime() {
+	  return creationTime;
+  }
+  
+  
+}
